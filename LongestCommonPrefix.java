84c5e6ff9ec0de73eb3b608367a76b423008fd67
Phillip Jones <pajonesvpbe@gmail.com>
2022-04-27 23:38:51 -0400

    Current solution works but is super gross
    Just look at this mess. This is what I get for attempted this after a full day of work. It... works, I guess. But at what cost

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -8,13 +8,27 @@ class Solution {
                 shortestLength = strs[i].length();
                 shortestIndex = i;
             }
-            System.out.println(shortestLength);
         }
         
         String lcPrefix = "";
         
-        for(int j = 0; j < shortestLength; j++ ){
-            
+        if(strs[shortestIndex].length() > 0){
+            lcPrefix = strs[shortestIndex].substring(0, 1);
+        }
+        
+        
+        for(int k = 0; k < shortestLength; k++){
+            System.out.println(lcPrefix);
+            for(int j = 0; j < strs.length; j++){
+                if(strs[j].startsWith(lcPrefix) == false){
+                    return lcPrefix.substring(0, lcPrefix.length()-1);
+                }
+            }
+            if (lcPrefix.length() >= shortestLength){
+                return lcPrefix;
+            } else {
+                lcPrefix = strs[shortestIndex].substring(0, lcPrefix.length()+1);
+            }
             
         }
         
