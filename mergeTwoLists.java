e46dd433f4596c0765bf0960d87ea67cb6b01969
Phillip Jones <pajonesvpbe@gmail.com>
2022-05-09 23:23:22 -0400

    Current work on this one
    Doesn't work. Fails at line 32 due to NullPointerException (val is null in one of the test cases)
    Current idea is to abandon the recursive approach entirely and copy them to an array, sort the array, and populate a bunch of ListNodes with that

--- mergeTwoLists.java
+++ mergeTwoLists.java
@@ -9,9 +9,33 @@
  * }
  */
 class Solution {
+    ListNode head = new ListNode();
+    ListNode curr = new ListNode();
+    int iterCount = 0;
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         
+        //should it be done like this? Probably not, no. Did I do it anyway? Yes.
+        if(iterCount == 0 && list1.val >= list2.val){
+            head = new ListNode(list1.val, list1.next);
+            curr = head.next;
+            iterCount++;
+            mergeTwoLists(list1.next, list2);
+        } else if (iterCount == 0 && list1.val < list2.val){
+            head = new ListNode(list2.val, list2.next);
+            curr = head.next;
+            iterCount++;
+            mergeTwoLists(list1, list2.next);
+        }
         
-        
+        if(list1 == null && list2 == null){
+            return head;
+        } else if (list1.val >= list2.val){
+            curr = list1;
+            curr.next = mergeTwoLists(list1.next, list2);
+        } else if (list1.val < list2.val){
+            curr = list2;
+            curr.next = mergeTwoLists(list1, list2.next);
+        }
+        return head;
     }
 }
